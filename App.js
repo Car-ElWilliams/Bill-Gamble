bd3ed795642f06c804f55b6cba17c754cad7a2ec
Carel Williams <carel@live.se>
2021-06-20 22:32:19 +0200

    adds a splashscreen however do you want it?

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Asset } from 'expo-asset';
 import LoadingScreen from './components/LoadingScreen';
 import Context from './Context';
 import Home from './routes/Home.js';
 import InputSelections from './routes/InputSelections';
 import Chicken from './routes/Chicken';
 import Results from './routes/Results';
+import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Stack = createStackNavigator();
 
@@ -18,6 +20,14 @@ export default function App() {
 	const [billValue, setBillValue] = useState(true);
 	const [allPlayerNames, setAllPlayerNames] = useState('');
 	const [riskyLevel, setriskyLevel] = useState('');
+
+	const [isReady, setIsReady] = useState(false);
+
+	//!Functions
+
+	function cacheResourcesAsync() {
+		SplashScreen.hideAsync();
+	}
 
 	return (
 		<Context.Provider
@@ -33,32 +43,43 @@ export default function App() {
 			}}
 		>
 			<PaperProvider>
-				<NavigationContainer>
-					<Stack.Navigator screenOptions={{ headerShown: true }}>
-						{/*<View style={styles.container}>*/}
-						{progressDone && <Stack.Screen name='LoadingScreen' component={LoadingScreen} />}
-						<Stack.Screen
-							name='Home'
-							component={Home}
-							//options={{
-							//	title: 'My home',
-							//	headerStyle: { backgroundColor: 'red', display: 'none' },
-							//	headerTintColor: '#fff',
-							//	headerTitleStyle: {
-							//		fontWeight: 'bold',
-							//		fontSize: 25,
-							//	},
-							//}}
-						/>
-						<Stack.Screen name='InputSelections' component={InputSelections} />
-						<Stack.Screen name='Chicken' component={Chicken} />
-						<Stack.Screen name='Results' component={Results} />
-						{/*{progressDone && <LoadingScreen />}*/}
-						{/*{!progressDone && <Home />}*/}
-						{/*<StatusBar style='auto' />*/}
-						{/*</View>*/}
-					</Stack.Navigator>
-				</NavigationContainer>
+				<AppLoading
+					startAsync={() => cacheResourcesAsync()}
+					onFinish={() => setIsReady(true)}
+					onError={console.warn}
+				/>
+				{isReady && (
+					<NavigationContainer>
+						<Stack.Navigator screenOptions={{ headerShown: true }}>
+							{/*<View style={styles.container}>*/}
+							{progressDone && <Stack.Screen name='LoadingScreen' component={LoadingScreen} />}
+							{
+								//<Stack.Screen name='LoadingScreen-v2' component={AppLoading} />
+								//</Stack.Screen>
+							}
+							<Stack.Screen
+								name='Home'
+								component={Home}
+								//options={{
+								//	title: 'My home',
+								//	headerStyle: { backgroundColor: 'red', display: 'none' },
+								//	headerTintColor: '#fff',
+								//	headerTitleStyle: {
+								//		fontWeight: 'bold',
+								//		fontSize: 25,
+								//	},
+								//}}
+							/>
+							<Stack.Screen name='InputSelections' component={InputSelections} />
+							<Stack.Screen name='Chicken' component={Chicken} />
+							<Stack.Screen name='Results' component={Results} />
+							{/*{progressDone && <LoadingScreen />}*/}
+							{/*{!progressDone && <Home />}*/}
+							{/*<StatusBar style='auto' />*/}
+							{/*</View>*/}
+						</Stack.Navigator>
+					</NavigationContainer>
+				)}
 			</PaperProvider>
 		</Context.Provider>
 	);
